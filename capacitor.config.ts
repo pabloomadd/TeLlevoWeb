edc1921c215cf5ea5570db95048b466df4a85f17
pablogonzamad <pa.gonzalezm@duocuc.cl>
2023-12-09 17:35:40 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LlevoWeb',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
