a6d8f4ebaaa30dbffdaa0e220f3367a2591f88f9
pablogonzamad <pa.gonzalezm@duocuc.cl>
2023-12-11 09:37:03 -0300

    Casi Listo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,10 @@ export class HomePage {
 
   constructor() {}
 
+  irLink(){
+    const linkDescarga = 'https://drive.google.com/file/d/1BOEg_UPr2IgctEi_AhUPHf-QObtCxiRu/view?usp=drive_link'; // Reemplaza esto con tu URL de descarga directa
+
+    window.open(linkDescarga, '_blank'); // Abre la URL en una nueva pestaña
+  }
+
 }
